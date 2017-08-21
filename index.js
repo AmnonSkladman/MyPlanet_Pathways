1206f06604be84d5eef39c45f83f812f7b4d25e0
derrekchow <derrekdc@gmail.com>
2017-08-21 14:53:10 -0400

    added server stuff

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
 const express = require('express')
-const app = express()
+
 const bodyParser = require('body-parser')
 
+const { Client } = require('pg')
 
-console.log("hello world")
+const app = express()
+
+/*
+
+const client = new Client({
+  user: 'dbuser',
+  host: 'database.server.com',
+  database: 'mydb',
+  password: 'secretpassword',
+  port: 3211,
+})
+client.connect()
+
+client.query('SELECT NOW()', (err, res) => {
+  console.log(err, res)
+  client.end()
+})
+*/
 
 const server = app.listen(3000, () => {
 	console.log("listening on port 3000")
@@ -11,4 +29,10 @@ const server = app.listen(3000, () => {
 
 app.get('/', (req, res, next) => {
 	res.sendFile(__dirname + '/static/index.html')
+})
+
+app.get('/api/:id&:name', (req, res, next) => {
+	const data = req.params.name
+	console.log(data)
+	res.status(204).end()
 })
