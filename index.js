550f24416ab61612f061f2faf50409e48712487a
derrekchow <derrekdc@gmail.com>
2017-08-21 14:04:37 -0400

    added ps

--- index.js
+++ index.js
@@ -2,9 +2,21 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 
+const { Client } = require('pg')
 
-console.log("hello world")
+const client = new Client({
+  user: 'dbuser',
+  host: 'database.server.com',
+  database: 'mydb',
+  password: 'secretpassword',
+  port: 3211,
+})
+client.connect()
 
+client.query('SELECT NOW()', (err, res) => {
+  console.log(err, res)
+  client.end()
+})
 const server = app.listen(3000, () => {
 	console.log("listening on port 3000")
 })
