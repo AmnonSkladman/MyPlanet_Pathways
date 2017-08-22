25d9531b3cd5082a8f7533fc8dcbf8f2e082fcc8
Marco Beltempo <marco.beltempo@gmail.com>
2017-08-22 10:17:03 -0400

    Establish connection to database

--- index.js
+++ index.js
@@ -1,38 +1,33 @@
-const express = require('express')
+const express = require('express');
+const { Client } = require('pg');
+const app     = express();
+const bodyParser = require('body-parser');
+const db_conn = require('./db/index');
+const config = require('./config/config');
 
-const bodyParser = require('body-parser')
 
-const { Client } = require('pg')
+const server = app.listen(config.port, () => {
+  console.log(`Listening on port ${config.port}`);
+});
 
-const app = express()
-
-/*
-
-const client = new Client({
-  user: 'dbuser',
-  host: 'database.server.com',
-  database: 'mydb',
-  password: 'secretpassword',
-  port: 3211,
-})
-client.connect()
-
-client.query('SELECT NOW()', (err, res) => {
-  console.log(err, res)
-  client.end()
-})
-*/
-
-const server = app.listen(3000, () => {
-	console.log("listening on port 3000")
-})
 
 app.get('/', (req, res, next) => {
 	res.sendFile(__dirname + '/static/index.html')
 })
-
+/*
 app.get('/api/:id&:name', (req, res, next) => {
 	const data = req.params.name
 	console.log(data)
 	res.status(204).end()
 })
+
+
+app.get('/:id', (req, res, next) => {
+  db_conn.query('SELECT * FROM users WHERE id = $1', [id], (err, res) => {
+    if (err) {
+      return next(err)
+    }
+    res.send(res.rows[0])
+  })
+})
+*/
