157f1f21a26d5d91fc6ac1bd5944dfcddc84245c
Marco Beltempo <marco.beltempo@gmail.com>
2017-08-22 10:58:16 -0400

    Fixed db connection directory

---
+++ db/index.js
@@ -0,0 +1,20 @@
+const config = require('../config/config');
+const { Pool, Client } = require('pg');
+
+const pool = new Pool({
+    connectionString: config.connectionString,
+  });
+
+var test_qry = "SELECT * FROM toronto_site_oct_2016";
+  
+  pool.query(test_qry, (err, res) => {
+    console.log(err, res)
+    pool.end()
+  });
+  
+  const client = new Client({
+    connectionString: config.connectionString,
+  });
+
+  client.connect();
+  
--- index.js
+++ index.js
@@ -14,20 +14,3 @@ const server = app.listen(config.port, () => {
 app.get('/', (req, res, next) => {
 	res.sendFile(__dirname + '/static/index.html')
 })
-/*
-app.get('/api/:id&:name', (req, res, next) => {
-	const data = req.params.name
-	console.log(data)
-	res.status(204).end()
-})
-
-
-app.get('/:id', (req, res, next) => {
-  db_conn.query('SELECT * FROM users WHERE id = $1', [id], (err, res) => {
-    if (err) {
-      return next(err)
-    }
-    res.send(res.rows[0])
-  })
-})
-*/
