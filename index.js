c24ee3a0a5c695c9a7e35a7f87fd108e0cacb730
Marco Beltempo <marco.beltempo@gmail.com>
2017-08-23 16:16:24 -0400

    Server (#2)
    * CSS AND ASSETS AHHHH
    
    * added server stuff
    
    * in progress file upload
    
    * Establish connection to database.
    In order to establish a connection to the db you must create
    
    * Establish connection to database.
    In order to connect to the db you must place your personal connection string in ./config/config.js
    
    * Establish connection to database
    
    * Revert "Establish connection to database."
    
    This reverts commit 644d25dc5af3ae2c8238eb29bd77c3972875c092.
    
    * attempt to connect ot pg
    
    * gotfix
    
    * Fixed db connection directory
    
    * added post request
    
    * parsed json data
    
    * setup database conneciton
    
    * prep for bluemix
    
    * Server configuration updates
    
    * turned dataList into string
    
    * fixed sheet retreival
    
    * fix
    
    * query works
    
    * Working upload xlsx to json feature
    
    * added comments

--- index.js
+++ index.js
@@ -1,26 +1,58 @@
-const express = require('express')
-const app = express()
-const bodyParser = require('body-parser')
+const express = require('express');
+const { Client } = require('pg');
+const app = express();
+const bodyParser = require('body-parser');
+const db_conn = require('./db/index');
+const config = require('./config/config');
+const XLSX = require('xlsx');
 
-const { Client } = require('pg')
+const server = app.listen(config.port, () => {
+  console.log(`Listening on port ${config.port}`);
+});
 
-const client = new Client({
-  user: 'dbuser',
-  host: 'database.server.com',
-  database: 'mydb',
-  password: 'secretpassword',
-  port: 3211,
-})
-client.connect()
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
-client.query('SELECT NOW()', (err, res) => {
-  console.log(err, res)
-  client.end()
-})
-const server = app.listen(3000, () => {
-	console.log("listening on port 3000")
-})
-
+//sends the main homepage
 app.get('/', (req, res, next) => {
 	res.sendFile(__dirname + '/static/index.html')
 })
+
+//user uploads excel files
+app.post('/api/upload', (req, res) => {
+  //receives files in an array of base64 data and file name object
+  var dataList = req.body.data.map(sheet => {
+    //converts base64 data into json
+    var newSheet = XLSX.utils.sheet_to_json(Object.values(XLSX.read(sheet.content.split(',')[1], {type: 'base64'}).Sheets)[0])
+    return newSheet.map(item => {
+      //parses file name and adds information into row objects
+      item.location = sheet.name.split("_")[0]
+      console.log(item.location)
+      item.date = sheet.name.split("_")[2] + " " + sheet.name.split("_")[3]
+      return item
+    })
+  })
+  var result = ""
+  for (i in dataList) {
+    //converts dataList in string
+    result += JSON.stringify(dataList[i])
+  }
+  console.log(dataList)
+  res.end()
+})
+
+//api for getting data from database
+app.get('/api', (req, res, next) => {
+	const params = req.query;
+	var keys = []
+ 	Object.keys(params).forEach((i) => {
+ 		keys.push(i)
+ 	})
+  //gets data from dgatabase based on the user's query
+  db_conn.getFromDatabase(keys, params, (result) => {
+    console.log(result)
+    res.send(result)
+  });
+})
+
