c52c4d022986fc35480129d3aca4fbaba28a78ef
Marco Beltempo <marco.beltempo@gmail.com>
2017-08-25 09:26:55 +0000

    Bluemix Update

--- config/config.js
+++ config/config.js
@@ -1,5 +1,11 @@
 //listen to server port or port 3000 on localhost
 module.exports = Object.freeze({
+<<<<<<< Upstream, based on 65eaa01592cc6afebca4b8c7fa22d984f7bc0139
     port: parseInt(process.env.PORT || 3000, 10)
   });
   
+=======
+    port: parseInt(process.env.VCAP_APP_PORT || 3000, 10),
+    connectionString : ''
+  });
+>>>>>>> 66618e0 Bluemix Update
